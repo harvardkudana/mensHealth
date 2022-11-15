64b8c44fe141fef97cb8f12757f4f2c90c3f320f
santymendoza <jmendoza@college.harvard.edu>
2022-11-15 16:29:08 -0500

    added data and template

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 /* main JS file */
+let myBarChart;
+let myCirlceChart;
 
-console.log("Hello JS world!");
+let promises = [
+    d3.csv("data/wordData.csv"),
+    d3.csv("data/word-pairData.csv")
+]
+
+Promise.all(promises)
+	.then(function(data){
+		initMainPage(data)
+	})
+    .catch(function (err) {
+        console.log(err)
+    });
+
+function updateVisualization(orders) {
+	console.log(orders);
+
+}
+
+function initMainPage(dataArray){
+    // myBarChart = new MatrixTable('matrixDiv', dataArray[0])
+    console.log(dataArray[0])
+    console.log(dataArray[1])
+
+}
